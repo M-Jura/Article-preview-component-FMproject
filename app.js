f84dddcd03d77320041614904931ae8b1c5f5570
M-Jura <102591316+M-Jura@users.noreply.github.com>
2022-04-22 14:38:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const share = document.querySelectorAll(".share__icon");
+
+
+
+share[0].addEventListener("click", () => {
+    document.querySelector(".c-social").classList.toggle("social_display");
+});
+
+share[1].addEventListener("click", () => {
+    document.querySelector(".c-social").classList.toggle("social_display");
+});
